a658128d470867089ad1591999627c1e7a5404ce
GoosseH <h.goossens@student.vu.nl>
2025-06-24 14:28:30 +0200

    Update script_MT.sh

--- Multitask learning/script_MT.sh
+++ Multitask learning/script_MT.sh
@@ -21,8 +21,9 @@ epochs=(30)
 batch_sizes=(16)
 modelnames=('globalise/GloBERTise') #'FacebookAI/xlm-roberta-base'  #'google-bert/bert-base-multilingual-cased'
 model_types=('RoBERTa') #XLM-R #BERT
+data_directory=('path/to/where/data/is/stored') #path to locate data files
 
-DEST="/gpfs/home2/hgoossens/Documents/Multi/gloBERT" #where to save results on Snellius
+DEST="path/to/save/results" #where to save results on Snellius
 mkdir -p "$DEST"
 
 for learningrate in "${learningrates[@]}"
@@ -35,21 +36,24 @@ do
             do
                 for model_type in "${model_types}"
                 do
-                python MTL.py --learning_rate=$learningrate \
-                                        --epoch=$epoch \
-                                        --batch_size=$batch_size \
-                                        --model_checkpoint=$modelname \
-                                        --model_type=$model_type
-
-                
-                cp $TMPDIR/outputs/*.json "$DEST" 2>/dev/null
-                cp $TMPDIR/outputs/*.csv "$DEST" 2>/dev/null
-                cp $TMPDIR/outputs/*.txt "$DEST" 2>/dev/null
-                cp $TMPDIR/outputs/*.png "$DEST" 2>/dev/null 
-
-                # Checkpoint (already limited to one)
-                cp -r $TMPDIR/outputs/checkpoint-* "$DEST" 2>/dev/null
-
+                    for directory in "${data_directory}"
+                    do
+                    python MTL.py --learning_rate=$learningrate \
+                                            --epoch=$epoch \
+                                            --batch_size=$batch_size \
+                                            --model_checkpoint=$modelname \
+                                            --model_type=$model_type \
+                                            --directory=$directory 
+    
+                    
+                    cp $TMPDIR/outputs/*.json "$DEST" 2>/dev/null
+                    cp $TMPDIR/outputs/*.csv "$DEST" 2>/dev/null
+                    cp $TMPDIR/outputs/*.txt "$DEST" 2>/dev/null
+                    cp $TMPDIR/outputs/*.png "$DEST" 2>/dev/null 
+    
+                    # Checkpoint (already limited to one)
+                    cp -r $TMPDIR/outputs/checkpoint-* "$DEST" 2>/dev/null
+                    done
                 done
             done
         done
